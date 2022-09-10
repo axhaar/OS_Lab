1257887ef8ca57274e31b6b7de1418dc2096ae6d
Achintya Mishra <76170002+axhaar@users.noreply.github.com>
2022-09-10 18:41:08 +0530

    Update fork.c

--- fork.c
+++ fork.c
@@ -1,14 +1,9 @@
-//exp write a c program to demonstrate the use of fork() system call 
-
 #include<stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-int main(){
-	int n;
-	printf("fork function is used to divide the process p1 into two process p1,p2 it returns 0 when successfully ");
+#include<unistd.h>
+
+int main()
+{
 	fork();
-	for(int i=0;i<100;i++){
-		printf("%d\n",getpid());
-	}
-	printf("\n");
+	printf(“Process ID: %d\n”, getpid());
+	return 0;
 }
