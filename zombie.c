779c9909c42158676d1890c4f4daea37d00aeb13
Achintya Mishra <76170002+axhaar@users.noreply.github.com>
2022-09-10 19:10:39 +0530

    Update zombie.c

--- zombie.c
+++ zombie.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main()
 {
-	int x=0;
-	x=fork();
-	if(x!=0)
-	{
-		sleep(2);
-		printf("parent\n");
-	}
-	else
-	{
-		printf("child\n");
-		exit(0);
-	}
-	return 0;
+    // Fork returns process id
+    // in parent process
+    pid_t child_pid = fork();
+  
+    // Parent process 
+    if (child_pid > 0)
+        sleep(50);
+  
+    // Child process
+    else        
+        exit(0);
+  
+    return 0;
 }
