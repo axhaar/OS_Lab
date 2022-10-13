f1d14b13739bb4299d1ddd28ed9430a95611cf9d
Achintya Mishra <76170002+axhaar@users.noreply.github.com>
2022-10-13 20:18:38 +0530

    even/odd

--- fork2.c
+++ fork2.c
@@ -1,31 +1,52 @@
 //write a c program in which the parent process computes the sum off even and child process computes the sum of odd numbers stored in array first ,the child process should print its oun process ie sum off odd numbers then the parent  
-#include<stdio.h>
-
+#include <stdio.h>
 #include <unistd.h>
-int main(){
-	int n=10,sum=0,sum1=0;
-	printf("fo ");
-	int arr[]={1,2,3,4,5,6,7,8,9,10};
-	
-	int k=fork();
-	if(k>0){
-		for(int i=0;i<n;i++){
-			if(arr[i]%2==0){
-				sum+=arr[i];
-			}
-		}
-		printf("%d\n",sum);
-	}
-	else{
-		for(int i=0;i<n;i++){
-			if(arr[i]%2==1){
-				sum1+=arr[i];
-			}
-		}
-		printf("%d\n",sum1);
-	}
-	printf("\n");
-return 0;
+
+int main()
+{
+
+    int pid, n;
+    printf("enter the no. of terms in the array : ");
+    scanf("%d", &n);
+    int a[n], sum = 0, status;
+
+    printf("\n enter vlues in the array : ");
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    pid = fork();
+    if (pid != 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] % 2 != 0)
+            {
+                sum += a[i];
+            }
+        }
+        printf("sum is odd number %d \n", sum);
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] % 2 == 0)
+            {
+                sum = sum + a[i];
+            }
+        }
+        printf("sum of even number =%d\n", sum);
+    }
+    return 0;
 }
+
+
+// write a c program in which parent process compute the sum of even process and child process computes the sum of odd numbers .
+//  stored in an array using a fork.
+
+// first , the child process should print its ans i.e sum of odd numbers then the parent should print its ans i.e the sum of even numbers
+
+// if pid==0
 
 
